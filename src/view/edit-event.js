f1354b3bdceb8420ccda043fd4510504bb3754ff
Artem <theuselessn@gmail.com>
2021-07-29 18:06:04 +0600

    module1-task3 eslint fixes

--- src/view/edit-event.js
+++ src/view/edit-event.js
@@ -1,5 +1,5 @@
 export const createEditEventTemplate = () => (
- `<li class="trip-events__item">
+  `<li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
                   <div class="event__type-wrapper">
